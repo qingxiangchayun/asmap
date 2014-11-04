ae5ca8d026264af6ff455b14cf9e206099681a4c
qingxiangchayun <yuanli891212@163.com>
2014-11-04 19:42:04 +0800

    db

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,26 @@ router.get('/', function(req, res) {
 // 数据库操作
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.3.133:27017/asmap_db');
+//mongoose.connect('mongodb://192.168.3.133:27017/asmap_db');
+var db = mongoose.createConnecton('mongodb://192.168.3.133:27017','asmap_db')
+
+//var db = mongoose.connection;
+
+db.on('error',console.error.bind(console,'连接错误');
+
+db.once('open',function(){
+	console.log('db connect ok');
+});
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var ASMap = new Schema({
+	id : ObjectId,
+	url : String,
+	link : String,
+	date : Date
+});
 
 
 // 获取列表
